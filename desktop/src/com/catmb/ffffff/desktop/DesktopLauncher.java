8b2f5034a1465255672c9b26d2ef1ab8141d841a
Rirush <endernyasha11@gmail.com>
2016-12-24 19:04:38 +0800

    Inital commit

---
+++ desktop/src/com/catmb/ffffff/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.catmb.ffffff.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.catmb.ffffff.ffffff;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ffffff(), config);
+	}
+}
---
+++ html/src/com/catmb/ffffff/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.catmb.ffffff.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.catmb.ffffff.ffffff;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ffffff();
+        }
+}
