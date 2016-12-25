b350318a38085e5540e141708db356faf27e1a21
YoungSenpai <youngsenpai@mail.ru>
2016-12-25 16:29:41 -0500

    Camera and window properties

--- core/src/com/catmb/ffffff/ffffff.java
+++ core/src/com/catmb/ffffff/ffffff.java
@@ -5,24 +5,52 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+import java.awt.*;
 
 public class ffffff extends ApplicationAdapter implements InputProcessor {
+
+
 	SpriteBatch batch;
+	Texture img;
+
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
+
+		int width = Gdx.graphics.getWidth();
+		int height = Gdx.graphics.getHeight();
+
 		batch = new SpriteBatch();
+
+		img = new Texture(Gdx.files.internal("badlogic.jpg"),true);
+
+		camera = new OrthographicCamera(width, height);
+		camera.position.set(new Vector3(width / 2, height / 2, 0));
+
 		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
+
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
+
 		batch.begin();
-		//batch.draw(img, 0, 0);
+		{
+			batch.draw(img, 0, 0);
+		}
 		batch.end();
 	}
 
--- desktop/src/com/catmb/ffffff/desktop/DesktopLauncher.java
+++ desktop/src/com/catmb/ffffff/desktop/DesktopLauncher.java
@@ -8,5 +8,11 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new ffffff(), config);
+
+		config.width = 800;
+		config.height = 600;
+		config.resizable = false;
+		config.title = "ff ff ff";
+
 	}
 }
