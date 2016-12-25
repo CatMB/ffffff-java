7e930dabe70ee6dce39f4f16e2f5637fb4cfdb29
YoungSenpai <youngsenpai@mail.ru>
2016-12-25 17:02:55 -0500

    test

--- core/src/com/catmb/ffffff/ffffff.java
+++ core/src/com/catmb/ffffff/ffffff.java
@@ -34,6 +34,7 @@ public class ffffff extends ApplicationAdapter implements InputProcessor {
 
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		System.out.println("Hello");
 		batch.begin();
 		//batch.draw(img, 0, 0);
 		batch.end();
