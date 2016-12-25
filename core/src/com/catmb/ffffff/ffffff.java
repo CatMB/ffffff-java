011e9401c976cb686a0b4945231d6dc2048bd8ab
YoungSenpai <youngsenpai@mail.ru>
2016-12-25 16:53:22 -0500

    Camera adding

--- core/src/com/catmb/ffffff/ffffff.java
+++ core/src/com/catmb/ffffff/ffffff.java
@@ -5,22 +5,33 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 
 public class ffffff extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Controls control;
+
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		control = Controls.getInstance();
 		Gdx.input.setInputProcessor(control);
+
+		camera = new OrthographicCamera(800, 600);
+		camera.position.set(new Vector3(400, 300, 0));
 	}
 
 	@Override
 	public void render () {
+
+	    camera.update();
+	    batch.setProjectionMatrix(camera.combined);
+
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
