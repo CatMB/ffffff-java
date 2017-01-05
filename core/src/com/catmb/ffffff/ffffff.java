fa5579958a728514cce04bacd3c3b6c092eea86b
Rirush <Rirush@cyberdude.com>
2017-01-05 19:44:53 +0800

    Renamed Level to Room, created rooms ArrayList

--- core/src/com/catmb/ffffff/ffffff.java
+++ core/src/com/catmb/ffffff/ffffff.java
@@ -1,38 +1,37 @@
 package com.catmb.ffffff;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.math.Vector3;
+
+import java.util.ArrayList;
 
 public class ffffff extends ApplicationAdapter{
 	SpriteBatch batch;
 	Controls control;
-
-	Level level;
+	ArrayList<Room> rooms;
+	Room room;
 	OrthographicCamera camera;
 	Hero hero;
+	int roomIterator;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		control = Controls.getInstance();
 		hero = Hero.getInstance();
-
+		roomIterator = 0;
 		Gdx.input.setInputProcessor(control);
 
 		camera = new OrthographicCamera(400, 300);
 		camera.position.set(new Vector3(200, 150, 0));
 		camera.update();
-		level = new Level("level/1.tmx");
-
+		room = new Room("level/1.tmx");
+		rooms = new ArrayList<Room>();
+		rooms.add(room);
 	}
 
 	@Override
@@ -46,7 +45,7 @@ public class ffffff extends ApplicationAdapter{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		{
-			level.render(camera);
+			rooms.get(roomIterator).render(camera);
 		}
 		hero.move();
 		hero.render(batch);
@@ -63,6 +62,6 @@ public class ffffff extends ApplicationAdapter{
 	@Override
 	public void dispose () {
 		batch.dispose();
-		level.dispose();
+		room.dispose();
 	}
 }
